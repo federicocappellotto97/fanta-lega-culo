4207deaf958c7c1723c768a5cc1bfc9f0bb21067
federicocappellotto97 <federicocappellotto@gmail.com>
2026-08-23 12:27:06 +0200

    fix

--- app/api/telegram/route.ts
+++ app/api/telegram/route.ts
@@ -18,7 +18,9 @@ import {
 } from "@/lib/bot/helper";
 
 const BOT_TOKEN = process.env.BOT_TOKEN!;
-const SITE_URL = process.env.NEXT_PUBLIC_SITE_URL!;
+const SITE_URL = process.env.VERCEL_PROJECT_PRODUCTION_URL
+  ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+  : (process.env.NEXT_PUBLIC_SITE_URL ?? "http://localhost:3000");
 const BASE_URL = `https://api.telegram.org/bot${BOT_TOKEN}`;
 
 // ---------------------------------------------------------------------------
